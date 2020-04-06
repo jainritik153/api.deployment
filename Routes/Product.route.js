a66064859b7690a9d8944b670d7118f1d1b48fee
ritik <jainritik153@gmail.com>
2020-04-06 22:23:04 +0530

    commit

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -1,21 +1,34 @@
 const express =require("express")
 const router = express.Router()
 const Product=require('../Models/Product.model')
-router.get('/',(req,res,next)=>{
-    res.send("list pof products");
+router.get('/',async (req,res,next)=>{
+    try {
+        const results=await Product.find()
+        res.send(results)
+    } catch (error) {
+        console.log(error.message)
+    }
 })
 
-router.post('/',(req,res,next)=>{
+router.post('/',async (req,res,next)=>{
     console.log(req.body)
-    const product=new Product(req.body)
-    product.save()
-    .then(result=>{
-        console.log(result);
-        res.send(result)
-    })
-    .catch(err=>{
-        console.log(err.message);
-    })
+    try {
+        const product=new Product(req.body)
+        const result=await product.save()
+        res.send(result) 
+    } catch (error) {
+        console.log(error.message);
+    }
+    
+    // product.save()
+    // .then(result=>{
+    //     console.log(result);
+    //     res.send(result)
+    // })
+    // .catch(err=>{
+    //     console.log(err.message);
+    // })
+
 })
 
 router.get('/:id',(req,res,next)=>{
